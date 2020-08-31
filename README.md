455828a958b2f5d32a081c8051c80a3a9ef05f55
Blightbuster <moritz.scheve@gmx.de>
2020-08-31 05:09:27 +0200

    Rework repository

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
 # EfTIcons
+
 Icons from the game Escape from Tarkov
 
-# Contributing
+## Structure
+
+- ```name``` folder contains icons named after their name (no dupelicated files)
+- ```uid``` folder contains icons named after their uid (keys and others have are dupelicates)
+- ```create.py``` is a script to create the ```uid``` folder from the ```name``` folder
+- ```correlation.json``` is used by ```create.py``` to determine the icon to every uid
+
+## Contributing
+
 If you would like to add or update a icon, make sure it fulfills the following criteria:
 
-- Named after its item id
+- Named after its uid / name (check with ```correlation.json```)
 - PNG format
 - Transparent background
 - Width and height (-1) are a multiple of 63. (64x127, 190x442, ...)
-- Must be orianted as if presented in inventory
 
 ![Cotribution example][exampleImg]
 
-# Where to find icons
-Rendered icons (everything moddable) are cached inside:
-```
+## Where to find icons
+
+Rendered icons (moddable items) are cached inside:
+
+```text
 C:\Users\User\AppData\Local\Temp\Battlestate Games\EscapeFromTarkov\Icon Cache
 ```
 
-
-[exampleImg]: https://github.com/Blightbuster/EfTIcons/blob/master/example.png
+[exampleImg]: example.png
---
+++ create.py
@@ -0,0 +1,58 @@
+import json
+import os
+import shutil
+
+###
+# This script will create a folder with all icons named after their uid,
+# based on a json file which contains the required information.
+###
+
+# Folder containing named icons
+namedFolder = 'name'
+
+# Output folder for icons with uid names
+outputFolder = 'uid'
+
+# Set this to the old output folder if you want to create based on it
+baseFolder = 'icons'
+
+# Set this true if you always want to use the base icon when in doubt
+forceBase = False
+
+# Data used for matching
+# Must contain uid and icon
+correlationSet = 'correlation.json'
+
+with open('correlation.json', encoding='utf-8') as json_file:
+    items = json.load(json_file)
+    for item in items:
+        src = namedFolder + os.path.sep + item['icon']
+
+        if not os.path.exists(src):
+            print(item['uid'] + " - Could not find: " + src)
+
+            baseSrc = baseFolder + os.path.sep + item['uid'] + '.png'
+            if not os.path.exists(baseSrc):
+                print(item['uid'] + " - Could not find base source at: " + baseSrc)
+                continue
+
+            print(item['uid'] + " - Found base source at: " + baseSrc)
+
+            if not forceBase:
+                print(item['uid'] + " - Do you want to use this icon? [Y/n]")
+                if input().lower() == 'n':
+                    continue
+
+            try:
+                dest = outputFolder + os.path.sep + item['uid'] + '.png'
+                shutil.copyfile(baseSrc, dest)
+            except Exception as e:
+                print(item['uid'] + " - Error while copying icon: " + str(e))
+
+            continue
+
+        try:
+            dest = outputFolder + os.path.sep + item['uid'] + '.png'
+            shutil.copyfile(src, dest)
+        except Exception as e:
+            print(item['uid'] + " - Error while copying icon: " + str(e))
